cdef9b6d746a7f8af8b7db0b79a70d950efea45e
Omegashinobi <robin.higham90@gmail.com>
2024-06-25 14:32:43 +0100

    added new sprites

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import Phaser, { Scale } from "phaser";
+import Phaser, { Scale, Scene } from "phaser";
 import BaseScene from "./scene/base/base";
 import Level1 from "./scene/level1";
 
@@ -6,12 +6,12 @@ let scenes = [
     new Level1()
 ]
 
-export const MAX_SIZE_WIDTH_SCREEN = 1920
-export const MAX_SIZE_HEIGHT_SCREEN = 1080
-export const MIN_SIZE_WIDTH_SCREEN = window.innerWidth
-export const MIN_SIZE_HEIGHT_SCREEN = window.innerHeight
-export const SIZE_WIDTH_SCREEN = 800
-export const SIZE_HEIGHT_SCREEN = 600
+export const MAX_SIZE_WIDTH_SCREEN = 640;
+export const MAX_SIZE_HEIGHT_SCREEN = 480;
+export const MIN_SIZE_WIDTH_SCREEN = window.innerWidth;
+export const MIN_SIZE_HEIGHT_SCREEN = window.innerHeight;
+export const SIZE_WIDTH_SCREEN = 640;
+export const SIZE_HEIGHT_SCREEN = 480;
 
 let parentContainer = document.getElementsByTagName("body").item(0);
 
@@ -24,22 +24,15 @@ const config = {
         parent: parentContainer,
         width: SIZE_WIDTH_SCREEN,
         height: SIZE_HEIGHT_SCREEN,
-        min : {
-            width : MIN_SIZE_WIDTH_SCREEN,
-            height :  MIN_SIZE_HEIGHT_SCREEN
-        },
-        max : {
-            width : MAX_SIZE_WIDTH_SCREEN,
-            height :  MAX_SIZE_HEIGHT_SCREEN
-        }
     },
     physics: {
         default: 'arcade',
         arcade: {
             gravity: { y: 0 },
-            debug: true
+            debug: false
         }
     },
+    pixelArt: true
 }
 
 export const app = new Phaser.Game(config);
